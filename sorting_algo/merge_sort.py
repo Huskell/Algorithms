b6ef3bb5a67e5f1f53a84658baa218f61c4d61d6
Huskell <mihaylov.ne@gmail.com>
2019-11-17 17:42:45 +0300

    6 commit

--- sorting_algo/merge_sort.py
+++ sorting_algo/merge_sort.py
@@ -4,17 +4,16 @@ from random import randint
 N = [randint(0, 100) for _ in range(10)]
 
 def merge_sort(N):
-
     if len(N) > 1:
-        mid = len(N)//2
-        right = N[mid:]
+        mid = len(N)//2 # определяем середину листа
+        right = N[mid:] # разрезаем на половины
         left = N[:mid]
 
-        merge_sort(right)
+        merge_sort(right)   # рекурсивно продолжаем разбивать до Len(list) = 1
         merge_sort(left)
 
         i, j, k = 0, 0, 0
-        while i < len(right) and j < len(left):
+        while i < len(right) and j < len(left):  # начинаем объединять половины в один лист
             if left[j] > right[i]:
                 N[k] = right[i]
                 i += 1
@@ -23,12 +22,12 @@ def merge_sort(N):
                 j += 1
             k += 1
 
-        while i < len(right):
+        while i < len(right): # добавляем остатки листа в конец списка
             N[k] = right[i]
             i += 1
             k += 1
 
-        while j < len(left):
+        while j < len(left): # добавляем остатки листа в конец списка
             N[k] = left[j]
             j += 1
             k += 1
---
+++ sorting_algo/quick_sort.py
@@ -0,0 +1,25 @@
+from random import randint, choice
+
+
+N = [randint(0, 100) for i in range(10)]
+
+def quick_sort(N):
+
+    if len(N) <= 1:
+        return N
+
+    q = choice(N)
+    left, mid, right = [], [], []
+
+    for n in N:
+        if n > q:
+            right.append(n)
+        elif n < q:
+            left.append(n)
+        else:
+            mid.append(n)
+    return quick_sort(left) + mid + quick_sort(right)
+
+print(N)
+print(quick_sort(N))
+

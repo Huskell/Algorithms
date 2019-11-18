c3577ac8e1664170afbe3372e5b4be1fd63ad5ea
Huskell <mihaylov.ne@gmail.com>
2019-11-18 20:04:40 +0300

    7 commit

--- sorting_algo/heap_sort.py
+++ sorting_algo/heap_sort.py
@@ -1,17 +1,14 @@
 from random import randint
 from data_str.heap import bin_heap
 
-N = [randint(0,100) for _ in range(10)]
+N = [randint(0,100) for _ in range(10000)]
 
 def heap_sort(N):
     m = len(N)
     arr2 = []
     while m > 0:
         arr = bin_heap().buildHeap(N)
-        max = arr[0]
-        arr[0] = 0
-        arr2.append(max)
-
+        arr2.append(arr.pop(0))
         m -= 1
     return arr2
 

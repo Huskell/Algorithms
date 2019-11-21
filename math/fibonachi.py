6e5bfc81f3fe6796b9fc4fe3dc7907f8bd9babd2
Huskell <mihaylov.ne@gmail.com>
2019-11-21 18:05:26 +0300

    10 commit

--- math/fibonachi.py
+++ math/fibonachi.py
@@ -1,3 +1,5 @@
+import numpy as np
+from numpy import linalg
 
 def fib(n):
     assert n >=0, 'Требуется положительное число n'
@@ -19,6 +21,23 @@ def fib_list(n):
         f.append(f[i-1]+f[i-2])
     return f
 
+def fib_rec(n):
 
-print(fib(8))
-print(fib_list(8))
+    if n < 2:
+        return n
+
+    return fib_rec(n - 1) + fib_rec(n - 2)
+
+def fib_matrix(n):
+
+    mat = np.array([[1, 1], [1, 0]], dtype=object)
+    number = linalg.matrix_power(mat,n-1)
+
+    return number[0, 0]
+
+
+
+print(fib(41))
+print(fib_list(41))
+print(fib_rec(41))
+print(fib_matrix(41))

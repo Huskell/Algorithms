53a42c49264d8ccbdac33cd25d7588f7848d9ddb
Huskell <mihaylov.ne@gmail.com>
2019-11-23 21:48:10 +0300

    11 commit

---
+++ graph_algo/dfs.py
@@ -0,0 +1,26 @@
+
+
+n = 10
+adj_list = [[2, 4, 6],
+            [9],
+            [0, 3],
+            [2, 4],
+            [0, 3],
+            [],
+            [0, 7, 8],
+            [6],
+            [6],
+            [1]]
+s = 0 #начало пути
+
+visited = [False] * n  # массив посещенных вершин
+
+def dfs(v):
+    visited[v] = True
+    for w in adj_list[v]:
+        if visited[w] == False:  # посещён ли текущий сосед?
+            print('w=', w)
+            dfs(w)
+
+print(dfs(s))
+print(visited)
